ece0e4c1c568f76ba2cccd7bbedbf49b4b6a1734
lakhan <manualsplash@gmail.com>
2019-04-27 20:26:41 +0530

    Create page header component
    This component contains the menu toggle for pages

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle
+} from "@ionic/react";
+
+interface PageHaderProps {
+  title: string;
+}
+
+const PageHeader: React.SFC<PageHaderProps> = ({ title }) => {
+  return (
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonButtons slot="start">
+          <IonMenuButton />
+        </IonButtons>
+        <IonTitle>{title}</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  );
+};
+
+export default PageHeader;
