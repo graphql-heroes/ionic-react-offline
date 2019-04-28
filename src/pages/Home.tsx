9be5e7b625a4be317450f9d3c1b5132a83c1105a
Lakshan Karunathilake <manualsplash@gmail.com>
2019-04-28 23:06:22 +0530

    Create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { IonPage } from "@ionic/react";
+import PageHeader from "../components/PageHeader";
+
+export class Home extends Component {
+  render() {
+    return (
+      <IonPage>
+        <PageHeader title="home" />
+      </IonPage>
+    );
+  }
+}
+
+export default Home;
