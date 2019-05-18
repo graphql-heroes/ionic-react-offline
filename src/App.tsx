459ac9061311138dce4ce507e66c6b69b2f58a56
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-18 23:35:39 +0530

    Add node-sass dependency
    For scss loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Menu from "./components/menu/Menu";
 import About from "./pages/About";
-import ManageTasks from "./pages/ManageTasks";
+import ManageTasks from "./pages/ManageTasks/ManageTasks";
 
 class App extends Component {
   render() {
@@ -14,8 +14,8 @@ class App extends Component {
             <Menu />
             <IonPage id="main">
               <Switch>
-                <Route path="/" component={About} />
-                <Route path="/tasks" component={ManageTasks} />
+                <Route path="/" component={ManageTasks} />
+                <Route path="tasks" component={About} />
               </Switch>
             </IonPage>
           </IonSplitPane>
